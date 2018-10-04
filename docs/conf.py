889e832baf59071d698db6ed7073952b9d01080d
Midnighter <midnighter@posteo.net>
2018-10-04 13:35:04 +0200

    Bump version: 0.2.3 → 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'Search String Parser'
 year = '2015'
 author = 'Moritz Emanuel Beber'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.2.3'
+version = release = '0.3.0'
 import sphinx_py3doc_enhanced_theme
 html_theme = "sphinx_py3doc_enhanced_theme"
 html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
--- src/searchstringparser/__init__.py
+++ src/searchstringparser/__init__.py
@@ -22,7 +22,7 @@ Search String Parser
 """
 
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 
 from .lexer import *  # NOQA
